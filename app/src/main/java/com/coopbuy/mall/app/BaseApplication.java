5e3d29eeb91d3d7ef73539cd74901bb1e654bba8
yangmingbin <1057207798@qq.com>
2017-07-21 11:23:25 +0800

    增加模块

--- app/src/main/java/com/coopbuy/mall/app/BaseApplication.java
+++ app/src/main/java/com/coopbuy/mall/app/BaseApplication.java
@@ -17,6 +17,7 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         baseApplication = this;
+
     }
 
     public static Context getAppContext() {
